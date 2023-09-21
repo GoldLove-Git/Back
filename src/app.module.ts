72d7a5b2ea2f60ee83680f2ee354f4b85ecd6b15
kkerast <dlwlsgkqkq@naver.com>
2023-09-21 15:16:09 +0900

    typeorm module 추가 및 환경변수설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { InfluencersModule } from './influencers/influencers.module';
 import { ChatModule } from './chat/chat.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, InfluencersModule, ChatModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      // synchronize: false,
+    }),
+    UsersModule,
+    InfluencersModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
