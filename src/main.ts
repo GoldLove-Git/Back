5e6cce440b28e32048c9764de2f4cd8582eb6e9c
kkerast <dlwlsgkqkq@naver.com>
2023-09-23 21:10:11 +0900

    Cors 설정 변경

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(80);
 }
 bootstrap();
- 
